b03aa780c4a3545d18bc2b5677e8e6a3c1a998b5
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-09-05 16:55:34 -0400

    Deloverable 1 Step 50

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,6 +5,7 @@ var y = window.innerHeight / 2;
 
 Leap.loop(controllerOptions, function(frame)
 {
+    clear();
     var cir = circle(x,y,50);
     //adds -1 or 0 or 1 to x
     x += Math.floor(Math.random() * 3) -1;
