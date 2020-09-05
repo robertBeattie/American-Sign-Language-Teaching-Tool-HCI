c7ef7e5458edd657b211c1a3ca16c1d610c1c17a
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-09-05 16:54:31 -0400

    Deliverable 1 Step 49

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,8 +1,13 @@
 var controllerOptions = {};
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
+
+
 Leap.loop(controllerOptions, function(frame)
 {
-    circle(x,y,50);
+    var cir = circle(x,y,50);
+    //adds -1 or 0 or 1 to x
+    x += Math.floor(Math.random() * 3) -1;
+
 }
 );
