08836e0f1c40343926c3c289b34bd0dc36df25f3
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-09-05 16:39:58 -0400

    Deliverable 1 Step 47

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,8 +1,8 @@
 var controllerOptions = {};
-var i = 0;
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
 Leap.loop(controllerOptions, function(frame)
 {
-    console.log(i);
-    i++;
+    
 }
 );
