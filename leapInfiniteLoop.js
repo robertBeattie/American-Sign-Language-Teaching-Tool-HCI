562733d9791ddb3395deff025082baa72204fdd9
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-09-05 17:37:20 -0400

    Deliverable 1 Step 66

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,17 +5,18 @@ var y = window.innerHeight / 2;
 
 Leap.loop(controllerOptions, function(frame)
 {
-    HandleFrame(frame);  
-});
-
-function HandleFrame(frame){
-/*
+    
     clear();
+    /*
     var cir = circle(x,y,50);
     //adds -1 or 0 or 1 to x
     x += Math.floor(Math.random() * 3) -1;
     y += Math.floor(Math.random() * 3) -1;
     */
+    HandleFrame(frame);  
+});
+
+function HandleFrame(frame){
    if(frame.hands.length === 1){
     var hand = frame.hands[0];
     HandleHand(hand);    
