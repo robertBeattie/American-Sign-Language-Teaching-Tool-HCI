35f33f65642de68523d29f0c816dc25d52985a53
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-09-05 18:03:03 -0400

    Deliverable 1 Step 71

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,10 +3,10 @@ var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
 var z = 0;
 
-var rawXMin = 100;
-var rawXMax = 0;
+var rawXMin = 200;
+var rawXMax = -200;
 
-var rawYMin = 100;
+var rawYMin = 400;
 var rawYMax = 0;
 
 Leap.loop(controllerOptions, function(frame)
@@ -33,7 +33,13 @@ function HandleFinger(finger){
         if(finger.id % 10 === 1){
            x = finger.tipPosition[0];  
            y = finger.tipPosition[1];  
-           z = finger.tipPosition[2];  
+           z = finger.tipPosition[2]; 
+
+           //check bounds
+           if(x < rawXMax){ x = rawXMax;}
+           if(x > rawXMin){ x = rawXMin;}
+           if(y < rawYMax){ y = rawYMax;}
+           if(y > rawYMin){ y = rawYMin;}
 
            console.log(finger);
            var cir = circle(x + window.innerWidth / 2,-y *2+ window.innerHeight ,50);   
