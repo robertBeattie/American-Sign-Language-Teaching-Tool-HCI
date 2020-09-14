24f4094be3b8e8430b72c90c41a4a499393c27fc
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-09-14 08:21:07 -0400

    Deliverable 2 Step 5

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -23,30 +23,30 @@ function HandleFrame(frame){
     }
 }
 function HandleHand(hand){
-        var fingers = hand.fingers;
-        fingers.forEach( finger => {
-            HandleFinger(finger);
-        });     
+    var fingers = hand.fingers;
+    fingers.forEach( finger => {
+        HandleFinger(finger);
+    });     
 }
 function HandleFinger(finger){
     //checks if finger id {0-4} ends in 1 if so its an index finger
-        if(finger.id % 10 === 1){
-           x = finger.tipPosition[0];  
-           y = finger.tipPosition[1];  
-           z = finger.tipPosition[2]; 
+        
+    x = finger.tipPosition[0];  
+    y = finger.tipPosition[1];  
+    z = finger.tipPosition[2]; 
 
-           //check bounds
-           if(x > rawXMax){ x = rawXMax;}
-           if(x < rawXMin){ x = rawXMin;}
-           x = Scale(x, rawXMin, rawXMax, 0, window.innerWidth);
-           if(y > rawYMax){ y = rawYMax;}
-           if(y < rawYMin){ y = rawYMin;}
-           y = Scale(y, rawYMin, rawYMax, 0, window.innerHeight);
+    //check bounds
+    if(x > rawXMax){ x = rawXMax;}
+    if(x < rawXMin){ x = rawXMin;}
+    x = Scale(x, rawXMin, rawXMax, 0, window.innerWidth);
+    if(y > rawYMax){ y = rawYMax;}
+    if(y < rawYMin){ y = rawYMin;}
+    y = Scale(y, rawYMin, rawYMax, 0, window.innerHeight);
 
-           console.log(x + "," + y);
-           var cir = circle(x,-y + innerHeight,50);   
+    console.log(x + "," + y);
+    var cir = circle(x,-y + innerHeight,50);   
 
-        }
+        
 }
 
 function Scale (OldValue, OldMin, OldMax, NewMin, NewMax){
