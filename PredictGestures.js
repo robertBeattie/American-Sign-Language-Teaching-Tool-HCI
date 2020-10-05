1097419792fb6686ecef6e7493145f5965fa6573
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-10-05 19:26:16 -0400

    Deliverable 5 Step 40

--- PredictGestures.js
+++ PredictGestures.js
@@ -1,12 +1,14 @@
 
 
 const knnClassifier = ml5.KNNClassifier();
-var testingSampleIndex = 1;
+var testingSampleIndex = 0;
 
 var frameIndex = 0;
 var frameflip = 0;
 var trainingCompleted = false;
 
+
+var predictedClassLabels = nj.zeros(3);
 function draw(){
     clear();
     
@@ -21,7 +23,7 @@ function Train(){
     //console.log("size :" + train0.shape[3]);
     for(var i =0; i < train0.shape[3]; i++){
         console.log(train0.pick(null,null,null,i,null).reshape(120).toString());
-        features = train0.pick(null,null,null,i,null).reshape(120); 
+        features = train0.pick(null,null,null,i,null).reshape(120).tolist(); 
         knnClassifier.addExample(features,0);
     
     }
@@ -31,4 +33,21 @@ function Train(){
 
 function Test(){
     //console.log("testing :" + test);
+    //console.log(test.pick(null,null,null,testingSampleIndex,null).reshape(120).tolist());
+    var currentFeatures = test.pick(null,null,null,testingSampleIndex,null).reshape(120);
+    console.log(currentFeatures.shape -1);
+    currentLabel = currentFeatures.get(currentFeatures.shape -1);
+    var predictedLabel = knnClassifier.classify(currentFeatures.tolist(),GotResults);  
+   
+    
+}
+
+function GotResults(err, result){
+    // console.log(testingSampleIndex + ": " + result.label);
+    predictedClassLabels.set(testingSampleIndex,result.label);
+
+    testingSampleIndex++;
+    if(testingSampleIndex >= train0.shape[3]){
+       testingSampleIndex = 0;
+    }
 }
