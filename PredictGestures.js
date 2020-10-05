5b4ccee4453c3516ad52de1f6e884ed1c3c3e2e7
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-10-05 19:57:06 -0400

    Deliverable 5 Step 55

--- PredictGestures.js
+++ PredictGestures.js
@@ -19,18 +19,20 @@ function draw(){
 }
 
 function Train(){
-    //console.log("training :" + train0);
+    console.log("training :");
     //console.log("size :" + train0.shape[3]);
     TrainHelper(train0,0);
     TrainHelper(train1,1);
-    
+    TrainHelper(train3,3);
+    TrainHelper(train4,4);
+
     trainingCompleted = true;
 }
 
 function Test(){
-    //console.log("testing :" + test);
+    console.log("testing :");
     //console.log(test.pick(null,null,null,testingSampleIndex,null).reshape(120).tolist());
-    var currentFeatures = test.pick(null,null,null,testingSampleIndex,null).reshape(120);
+    var currentFeatures = train4.pick(null,null,null,testingSampleIndex,null).reshape(120);
     currentLabel = currentFeatures.get(currentFeatures.shape -1);
     var predictedLabel = knnClassifier.classify(currentFeatures.tolist(),GotResults);  
    
@@ -41,14 +43,14 @@ function GotResults(err, result){
     predictedClassLabels.set(testingSampleIndex,result.label);
     
     testingSampleIndex++;
-    if(testingSampleIndex >= train0.shape[3]){
+    if(testingSampleIndex >= test.shape[3]){
        testingSampleIndex = 0;
     }
 }
 
 function TrainHelper(train,n){
     for(var i =0; i < train.shape[3]; i++){
-        console.log(train.pick(null,null,null,i,null).reshape(120).toString());
+        //console.log(train.pick(null,null,null,i,null).reshape(120).toString());
         features = train.pick(null,null,null,i,null).reshape(120).tolist(); 
         knnClassifier.addExample(features,n);
     }
