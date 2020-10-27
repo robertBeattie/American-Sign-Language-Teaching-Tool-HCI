9795794f7a742925551c18a8795c060910957959
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2020-10-26 21:51:30 -0400

    Deliverable 7 Step 16

--- PredictGestures.js
+++ PredictGestures.js
@@ -9,27 +9,28 @@ var trainingCompleted = false;
 var oneFrameOfData = nj.zeros([5,4,6]);
 
 var predictedClassLabels = nj.zeros(3);
-
+var extenedArray = nj.zeros(5);
 //predicton accuracy
 var n = 0;
 var m = 1;
 var c;
-var d = "8";
+var d = "0";
 
 Leap.loop(controllerOptions, function(frame)
 {
     clear();
-    if(!trainingCompleted){
-        Train();
-    }
+    //if(!trainingCompleted){Train();}
     //console.log(oneFrameOfData.toString());
     HandleFrame(frame);
 });
 
 function Train(){
+    var start = new Date().getTime();
     console.log("training :");
     //console.log("size :" + train0.shape[3]);
     
+    /*
+    Attempt 1
     TrainHelper(train0,0);
     TrainHelper(train1,1);
     TrainHelper(train2,2);
@@ -43,26 +44,48 @@ function Train(){
 
     TrainHelper(train0Robby,0);
     TrainHelper(train0Bongard,0);
+    TrainHelper(train0Allison,0);
 
-    TrainHelper(train1Bongard,1);
     TrainHelper(train1Robby,1);
+    TrainHelper(train1Allison,1);
+    TrainHelper(train1Bongard,1);
     TrainHelper(train1Davis,1);
     TrainHelper(train1McLaughlin,1);
     TrainHelper(train1Jimmo, 1);
     TrainHelper(train1Li, 1);
     TrainHelper(train1Rice, 1);
+    TrainHelper(train1Riofrio, 1);
+    TrainHelper(train1Nimako, 1);
+    TrainHelper(train1Hunt, 1);
 
     TrainHelper(train2Banaszewski, 2);
     TrainHelper(train2Bongard, 2);
     TrainHelper(train2Downs, 2);
-
+    TrainHelper(train2Robby, 2);
+    TrainHelper(train2Jing, 2);
 
     TrainHelper(train3Banaszewski, 3);
     TrainHelper(train3Bongard, 3);
+    TrainHelper(train3Jing, 3);
 
     TrainHelper(train4Bertschinger, 4);
     TrainHelper(train4Bongard, 4);
     TrainHelper(train4Faucher, 4);
+    TrainHelper(train4Kiely, 4);
+    TrainHelper(train4Liu, 4);
+    TrainHelper(train4Makovsky, 4);
+    TrainHelper(train4OBrien, 4);
+    TrainHelper(train4Makovsky, 4);
+    TrainHelper(train4OBrien, 4);
+    TrainHelper(train4Sheboy, 4);
+    TrainHelper(train4Socia, 4);
+    TrainHelper(train4Robby, 4);
+
+    TrainHelper(train5Bertschinger, 5);
+    TrainHelper(train5Faucher, 5);
+    TrainHelper(train5Blewett, 5);
+    TrainHelper(train5Fekert, 5);
+    TrainHelper(train5Robby, 5);
 
     TrainHelper(train7Bongard, 7);
     TrainHelper(train7Laquerre, 7);
@@ -74,8 +97,68 @@ function Train(){
     TrainHelper(train8ILee, 8);
     TrainHelper(train8JClark, 8);
 
+    TrainHelper(train9Bongard, 9);
+    TrainHelper(train9Goldman, 9);
+    TrainHelper(train9ILee, 9);
+*/
+
+
+    TrainHelper(train_0, 0);
+    TrainHelper(train0Allison,0);
+    TrainHelper(train_1, 1);
+    TrainHelper(train1Robby,1);
+    TrainHelper(train1Allison,1);
+    TrainHelper(train1Bongard,1);
+    TrainHelper(train1McLaughlin,1);
+
+    TrainHelper(train_2, 2);
+    TrainHelper(train2Banaszewski, 2);
+    TrainHelper(train2Bongard, 2);
+    TrainHelper(train2Downs, 2);
+
+    TrainHelper(train_3, 3);
+    TrainHelper(train3Bongard, 3);
+
+    TrainHelper(train_4, 4);
+    TrainHelper(train4Robby, 4);
+    TrainHelper(train4, 4);
+    TrainHelper(train4OBrien, 4);
+    //TrainHelper(train4Bongard, 4);
+
+    TrainHelper(train_5, 5);
+    //TrainHelper(train5Bongard, 5);
+
+    TrainHelper(train_6, 6);
+    TrainHelper(train6,6);
+
+    TrainHelper(train_7, 7);
+    TrainHelper(train_8, 8);
+    TrainHelper(train_9, 9);
+    
+   TrainHelper(train0Bongard, 0);
+   TrainHelper(train1Davis, 1);
+   TrainHelper(train2Liu, 2);
+   TrainHelper(train3Li, 3);
+   TrainHelper(train4Liu, 4);
+   TrainHelper(train5Shi, 5);
+   TrainHelper(train6Bongard, 6);
+   TrainHelper(train7, 7);
+   TrainHelper(train8Bongard, 8);
+   TrainHelper(train9JClark, 9);
+
+   TrainHelper(train7Bongard, 7);
+   TrainHelper(train7Laquerre, 7);
+  // TrainHelper(train7Manian, 7);
+   //TrainHelper(train7Vega, 7);
+
+   //TrainHelper(train8Goldman, 8);
+   TrainHelper(train8ILee, 8);
+   //TrainHelper(train8JClark, 8);
+   
     trainingCompleted = true;
-    console.log("training complete");
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log("training complete it took: ", time );
 }
 
 function Test(){
@@ -85,9 +168,96 @@ function Test(){
     currentLabel = currentFeatures.get(currentFeatures.shape -1);
 }
 
+function TestExtended(){
+    //0
+    if( extenedArray.get(0) === 0 &&
+        extenedArray.get(1) === 0 &&
+        extenedArray.get(2) === 0 &&
+        extenedArray.get(3) === 0 &&
+        extenedArray.get(4) === 0 ){
+            return 0;
+        }else
+    //1
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 0 &&
+    extenedArray.get(3) === 0 &&
+    extenedArray.get(4) === 0 ){
+        return 1;
+    }else
+    //2
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 0 &&
+    extenedArray.get(4) === 0 ){
+        return 2;
+    }else
+    //3
+    if( extenedArray.get(0) === 1 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 0 &&
+    extenedArray.get(4) === 0 ){
+        return 3;
+    }else
+    //4
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 1 &&
+    extenedArray.get(4) === 1 ){
+        return 4;
+    }else
+    //5
+    if( extenedArray.get(0) === 1 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 1 &&
+    extenedArray.get(4) === 1 ){
+        return 5;
+    }else
+    //6
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 1 &&
+    extenedArray.get(4) === 0 ){
+        return 6;
+    }else
+    //7
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 0 &&
+    extenedArray.get(4) === 1 ){
+        return 7;
+    }else
+    //8
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 1 &&
+    extenedArray.get(2) === 0 &&
+    extenedArray.get(3) === 1 &&
+    extenedArray.get(4) === 1 ){
+        return 8;
+    }else
+    //9
+    if( extenedArray.get(0) === 0 &&
+    extenedArray.get(1) === 0 &&
+    extenedArray.get(2) === 1 &&
+    extenedArray.get(3) === 1 &&
+    extenedArray.get(4) === 1 ){
+        return 9;
+    }else{
+        return 5;
+    }
+    
+}
+
 function GotResults(err, result){
     PredictionAccuracy(result.label);
-    console.log(m, c,);
+   // console.log(c);
+    console.log(TestExtended());
   //predictedClassLabels.set(testingSampleIndex,result.label);
 }
 
@@ -95,6 +265,10 @@ function TrainHelper(train,n){
     CenterData();
     for(var i =0; i < train.shape[3]; i++){
         //console.log(train.pick(null,null,null,i,null).reshape(120).toString());
+        features = train.pick(null,null,null,i,null).reshape(120).tolist(); 
+        knnClassifier.addExample(features,n);
+        
+        MirrorHand(train);
         features = train.pick(null,null,null,i,null).reshape(120).tolist(); 
         knnClassifier.addExample(features,n);
     }
@@ -114,10 +288,18 @@ function HandleFrame(frame){
      //Metacapals in palm
      var fingers = hand.fingers;
      for (var i=3; i >= 0; i--){
+       
          fingers.forEach( finger => {
              HandleFinger(finger, i, interactionBox);
+             if(finger.extended){
+                extenedArray.set(finger.type, 1);
+             }else{
+                extenedArray.set(finger.type, 0);
+             }
+             
          });
      }
+    // console.log(extenedArray.toString());
  }
  function HandleFinger(finger, boneType, interactionBox){
      
@@ -161,7 +343,7 @@ function HandleFrame(frame){
     line(nx,-ny + innerHeight,px, -py + innerHeight);
  
     CenterData();
-    Test();
+   // Test();
  }
 
  function TransformCoordinates (normalizedPosition){
@@ -175,6 +357,24 @@ function CenterData(){
     CenterXData();
     CenterYData();
     CenterZData();
+}
+function MirrorHand(frame){
+    var xValues = frame.slice([],[],[0,6,3]);
+    var currentMean = xValues.mean();
+    var horizontalShift = 0.5 - currentMean;
+    for(var currentRow = 0; currentRow < 5; currentRow++){
+        for(var currentColumn = 0; currentColumn < 4; currentColumn++){
+            currentX = frame.get(currentRow,currentColumn,0);
+            shiftedX = currentX + horizontalShift;
+            frame.set(currentRow,currentColumn,0, 1 - shiftedX);
+
+            currentX = frame.get(currentRow,currentColumn,3);
+            shiftedX = currentX + horizontalShift;
+            frame.set(currentRow,currentColumn,3,1 - shiftedX);
+        }
+    }
+    xValues = frame.slice([],[],[0,6,3]);
+    var currentMeanAfter = xValues.mean();
 }
 function CenterXData(){
     var xValues = oneFrameOfData.slice([],[],[0,6,3]);
